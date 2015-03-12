6e7c97637c1db35fdb09ce2dd31dff2c406efe61
khorshid1 <krahmaninejad2015@kellogg.northwestern.edu>
2015-03-12 14:49:40 -0500

    unclear

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :song
+  belongs_to :survey
   has_many :answers
   #has_many :questions, dependent: :destroy
   #has_many :songs, through: :questions
